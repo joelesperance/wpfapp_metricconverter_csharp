ad8dec2cc6cc72bdf03fd3d2ce7a796934ae774a
joelesperance <lesperance.joe@gmail.com>
2023-05-04 20:17:39 -0400

    Add project files.

---
+++ wpfapp_metricconverter_csharp/Controls/HWL.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfapp_metricconverter_csharp.Controls
+{
+    /// <summary>
+    /// Interaction logic for HWL.xaml
+    /// </summary>
+    public partial class HWL : UserControl
+    {
+        public HWL()
+        {
+            InitializeComponent();
+        }
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            double hMil = Utils.ParseInput(HMil.Text);
+            double wMil = Utils.ParseInput(WMil.Text);
+            double lMil = Utils.ParseInput(LMil.Text);
+
+            double hInches = ConvertToInches(hMil);
+            double wInches = ConvertToInches(wMil);
+            double lInches = ConvertToInches(lMil);
+
+            HInches.Text = hInches.ToString();
+            WInches.Text = wInches.ToString();
+            LInches.Text = lInches.ToString();
+            CCInches.Text = CalculateCrossCorner(wInches, lInches).ToString();
+            CCMil.Text = CalculateCrossCorner(wMil, lMil).ToString();
+            StInches.Text = CalculateSteelNumber(hInches, wInches, lInches).ToString();
+            StMil.Text = CalculateSteelNumber(hMil, wMil, lMil).ToString();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            HMil.Text = "0";
+            WMil.Text = "0";
+            LMil.Text = "0";
+            HInches.Text = "0";
+            WInches.Text = "0";
+            LInches.Text = "0";
+            CCInches.Text = "0";
+            CCMil.Text = "0";
+            StInches.Text = "0";
+            StMil.Text = "0";
+        }
+
+        private double ConvertToInches(double millimeter)
+        {
+            double inches = millimeter / 25.4;
+            return inches;
+        }
+
+        private double CalculateCrossCorner(double w, double l)
+        {
+            double crosscorner = Math.Sqrt((w * w) + (l * l));
+            return crosscorner;
+        }
+
+        private double CalculateSteelNumber(double h, double w, double l)
+        {
+            return Math.Round(h * w * l * 0.283, 3);
+        }
+    }
+}
---
+++ wpfapp_metricconverter_csharp/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfapp_metricconverter_csharp
+{
+    public class Utils
+    {
+        public static double ParseInput(string input)
+        {
+            double result;
+            if (!double.TryParse(input, out result))
+            {
+                result = 0;
+            }
+            return result;
+        }
+    }
+}
